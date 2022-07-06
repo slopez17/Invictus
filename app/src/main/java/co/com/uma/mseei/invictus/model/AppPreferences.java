d8d3d769a8e1b26219144a14bd7004c11a16fc8f
Sandra Marcela López Torres <sandramarcelal95@gmail.com>
2022-07-06 16:21:40 -0500

    completar fragment_settings

--- app/src/main/java/co/com/uma/mseei/invictus/model/AppPreferences.java
+++ app/src/main/java/co/com/uma/mseei/invictus/model/AppPreferences.java
@@ -7,24 +7,26 @@ public class AppPreferences {
 
     /**
      * Settings constants
-     * If you need to modify some value, you must change corresponding key value on
+     * If you need to modify some value, you must change corresponding key/default value on
      * fragment_settings
      **/
     public static final String PREF_AUTO_FINISH = "autoFinish";
-    public static final String PREF_MAX_SAMPLES_LIMIT = "maxSamplesLimit";
-    public static final String PREF_MAX_MEASURES_ON_MEMORY = "maxMeasuresMem";
+    public static final String PREF_SAMPLES_LIMIT = "samplesLimit";
+    public static final String PREF_SAMPLES_ON_MEMORY = "samplesOnMemory";
     public static final String PREF_SAVE_ON_SD = "saveOnSD";
     public static final String PREF_FILE_NAME = "fileName";
     public static final String PREF_MULTIPLE_FILES = "multipleFiles";
-    public static final String PREF_REFRESH_TIME = "refreshTimeInSeconds";
+    public static final String PREF_REFRESH_TIME = "refreshTime";
+    public static final String PREF_UNIT_SYSTEM = "unitSystem";
 
     public static final boolean DEFAULT_AUTO_FINISH = false;
-    public static final String DEFAULT_MAX_MEASURES_TO_ACQUIRE = "200";
-    private static final String DEFAULT_MAX_MEASURES_ON_MEMORY = "200";
+    public static final String DEFAULT_SAMPLES_LIMIT = "2000";
+    private static final String DEFAULT_SAMPLES_ON_MEMORY = "200";
     private static final boolean DEFAULT_SAVE_ON_SD = false;
-    private static final String DEFAULT_FILENAME = "Sporting";
+    private static final String DEFAULT_FILE_NAME = "Invictus";
     private static final boolean DEFAULT_MULTIPLE_FILES = false;
     private static final String DEFAULT_REFRESH_TIME = "5";
+    public static final String DEFAULT_UNIT_SYSTEM = "0";
 
 
 
